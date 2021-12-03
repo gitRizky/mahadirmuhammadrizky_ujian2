c8745b81995bc067133897fd1f074368dce32803
Lenovo <Lenovo@LAPTOP-NADE445V>
2021-12-03 14:45:46 +0700

    first commit

---
+++ src/MahadirMuhammadRizky_SQABatch1_Ujian2/No10/No10Main.java
@@ -0,0 +1,22 @@
+package MahadirMuhammadRizky_SQABatch1_Ujian2.No10;
+
+public class No10Main {
+	public static void main(String[] args) {
+		SMA sma = new SMA();
+		sma.setNama("sma bekasi");
+		sma.setAlamat("bekasi");
+		sma.setJenjang("sma");
+		sma.nama();
+		sma.alamat();
+		sma.jenjang();
+		System.out.println();
+		
+		SMK smk = new SMK();
+		smk.setNama("smk bekasi");
+		smk.setAlamat("bekasi");
+		smk.setJenjang("smk");
+		smk.nama();
+		smk.alamat();
+		smk.jenjang();
+	}
+}
