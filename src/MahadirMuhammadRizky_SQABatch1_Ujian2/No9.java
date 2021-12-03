2f4eb43915b036fc35c7f395e98ea42db3f299e8
Lenovo <Lenovo@LAPTOP-NADE445V>
2021-12-03 16:08:53 +0700

    ganti soal

--- src/MahadirMuhammadRizky_SQABatch1_Ujian2/No9.java
+++ src/MahadirMuhammadRizky_SQABatch1_Ujian2/No9.java
@@ -3,20 +3,39 @@ package MahadirMuhammadRizky_SQABatch1_Ujian2;
 import java.util.Scanner;
 
 public class No9 {
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		// Silakan isi bagian ini
+		int[] arr = new int[5];
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Masukan Jarak (kilometer) = ");
-		int jarak = scan.nextInt();
-		System.out.print("Masukan waktu (jam) = ");
-		int waktu = scan.nextInt();
-		
-		System.out.println("Kecepatan rata-rata anda adalah " + hitungKecepatan(jarak, waktu) + " km/jam");
-		
-	}
-		static double hitungKecepatan(double jarak,  double waktu) {
-		//Silakan isi bagian ini dan ganti “anu” dengan kode yang tepat
-			return jarak/waktu;
+		System.out.println("masukan 5 nilai yang ingin dijumlahkan : ");
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scan.nextInt();
 		}
+
+		System.out.println("hasil = " + jumlahArray(arr));
+
+		System.out.println();
+
+		System.out.print("nilai yang ingin dicek dalam array : ");
+		System.out.println("hasil = " + cariNilai(arr, scan.nextInt()));
+
+	}
+
+	public static int jumlahArray(int[] nilai) {
+		int sum = 0;
+		for (int i = 0; i < nilai.length; i++) {
+			sum += nilai[i];
+		}
+		return sum;
+	}
+
+	public static boolean cariNilai(int[] arr, int nilai) {
+		for (int x = 0; x < arr.length; x++) {
+			if (arr[x] == nilai)
+				return true;
+		}
+		return false;
+	}
 
 }
